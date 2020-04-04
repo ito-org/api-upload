f28f949077162e22c6b12ae0443f112b4bc7840e
calmandniceperson <mko.mko31@gmail.com>
2020-04-04 14:34:44 +0200

    Create functions for creating blueprints with database object

--- app/routes/v0/contacts.py
+++ app/routes/v0/contacts.py
@@ -1,11 +1,14 @@
 from flask import Blueprint, request, Response, abort, current_app
 from app.model import ApiError
+from app.persistence.db import DBConnection
 
 
-contacts = Blueprint("v0.contacts", __name__, url_prefix="/v0/contacts")
+def construct_contacts_blueprint(dbConn: DBConnection):
+    contacts = Blueprint("v0.contacts", __name__, url_prefix="/v0/contacts")
 
+    @contacts.route("/report", methods=["POST"])
+    def report() -> Response:
+        abort(501)
+        return Response(None, status=201)
 
-@contacts.route("/report", methods=["POST"])
-def report() -> Response:
-    abort(501)
-    return Response(None, status=201)
+    return contacts
