44f6b7ce6e481c2968d56c63ba344abb716f93dc
calmandniceperson <mko.mko31@gmail.com>
2020-04-04 14:36:41 +0200

    Create routes with new blueprint creation functions and pass database to them

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,13 @@
-from flask import Flask
-from app.persistence.db import mongo
 import os
+from flask import Flask
+from app.persistence.db import DBConnection
 
-from .routes.v0 import cases
-from .routes.v0 import contacts
+from .routes.v0 import construct_cases_blueprint
+from .routes.v0 import construct_contacts_blueprint
 
 
 def create_app() -> Flask:
     app = Flask(__name__)
-
-    app.register_blueprint(cases)
-    app.register_blueprint(contacts)
 
     env = os.environ.get("FLASK_ENV", "production")
     if env == "production":
@@ -23,5 +20,9 @@ def create_app() -> Flask:
         if mongo_uri is not None:
             app.config.update({"MONGO_URI": mongo_uri})
 
-    mongo.init_app(app)
+    dbConn = DBConnection(os.environ.get("MONGO_URI"))
+
+    app.register_blueprint(construct_cases_blueprint(dbConn))
+    app.register_blueprint(construct_contacts_blueprint(dbConn))
+
     return app
