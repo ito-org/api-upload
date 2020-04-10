007d71217a1a5352a2c93ad216c2b47084f61d32
Michael Koeppl <koeppl.michael@outlook.com>
2020-04-10 23:27:51 +0200

    Remove unused database functions

--- db/db.py
+++ db/db.py
@@ -2,7 +2,7 @@ from flask import Flask
 from pymongo import MongoClient  # type: ignore
 from typing import Optional, Dict, Any, Iterator, List
 from uuid import uuid4, UUID
-from datetime import datetime, timedelta
+from datetime import datetime
 import time
 from random import randrange
 from models.case import Case
@@ -18,37 +18,8 @@ class DBConnection:
         client = MongoClient(hostUri)
         self.db = client.ito
 
-    def insert_cases(self, cases: List[Any]) -> None:
-        self.db.cases.insert_many(cases)
+    def insert_reportsig(self, reportsig: str, timestamp: datetime) -> None:
+        self.db.reportsigs.insert_one({"reportsig": reportsig, "timestamp": timestamp})
 
-    def count_cases(self) -> int:
-        return int(self.db.cases.count_documents({}))
-
-    def insert_contacts(self, contacts: List[Contact]) -> None:
-        self.db.contacts.insert_many(contacts)
-        return
-
-    def get_cases(
-        self, uuid: UUID, lat: Optional[int] = None, lon: Optional[int] = None
-    ) -> Iterator[Case]:
-        # TODO: prevent timing attacks that could reveal if a UUID is present or not
-        conditions: Dict[str, Any] = {}
-        if lat is not None:
-            conditions["lat"] = lat
-        if lon is not None:
-            conditions["lon"] = lon
-        conditions["uuid"] = str(uuid)
-
-        # if last_case is None:
-        #    conditions["upload_timestamp"] = {"$gte": self.random_time_in_the_past()}
-        # else:
-        #    conditions["upload_timestamp"] = {"$gte": last_case["upload_timestamp"]}
-        return (case for case in self.db.cases.find(conditions))
-
-    def random_time_in_the_past(self) -> datetime:
-        # FIXME: use a cryptographically secure RNG
-        now: datetime = datetime.now()
-        one_day_ago: datetime = now - timedelta(days=1)
-        noise_minutes: int = randrange(start=0, stop=60 * 24 * 6, step=1)
-        # anything between 1 and 7 days ago
-        return one_day_ago - timedelta(minutes=noise_minutes)
+    def get_reportsigs(self) -> List[str]:
+        return list(self.db.reportsigs.find({}, {"_id": False}))
