8bbf0fe16e29fb9b34cb8c7eede9b119ff0c6643
Michael Koeppl <koeppl.michael@outlook.com>
2020-04-10 23:40:19 +0200

    Add provisional test for report handler

--- tests/v0/test_cases.py
+++ tests/v0/test_cases.py
@@ -1,54 +1,15 @@
-from flask import url_for
-from typing import List
-from uuid import uuid4, UUID
-from db import DBConnection
 import json
+from datetime import datetime
+from flask import url_for, Response
 from flask.testing import FlaskClient
-from flask import Response
-from itertools import repeat
-from random import uniform
-from datetime import datetime, timedelta
-from models.case import Case
-from models.contact import Contact
-import os
 
 
-def generate_random_cases(dbConn: DBConnection, n: int) -> List[Case]:
-    cases: List[Case] = []
-    for _ in repeat(None, n):
-        cases.append(
-            Case(
-                uuid4(),
-                round(uniform(-90, 90), 1),
-                round(uniform(-180, 180), 1),
-                1,
-                dbConn.random_time_in_the_past(),
-            )
-        )
-    return cases
-
-
-def test_insert(client: FlaskClient):
-    dbConn = DBConnection(os.environ.get("MONGO_URI"))
-    # TODO: actually test that case is inserted
-    prev_count: int = dbConn.count_cases()
-    n: int = 10
-    cases: list = generate_random_cases(dbConn, n)
+def test_report_post(client: FlaskClient):
+    data = {
+        "reportsig": "teststr",
+        "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+    }
     res: Response = client.post(
-        url_for("v0.cases.report"),
-        data=json.dumps([case.__dict__ for case in cases], cls=CaseEncoder),
-        content_type="application/json",
+        url_for(".report"), data=json.dumps(data), content_type="application/json"
     )
-    assert res.status_code == 201
-    assert dbConn.count_cases() == (prev_count + n)
-
-
-# copied from https://stackoverflow.com/a/48159596/9926795
-class CaseEncoder(json.JSONEncoder):
-    def default(self, obj):
-        if isinstance(obj, UUID):
-            # if the obj is uuid, we simply return the value of uuid
-            return obj.hex
-        if isinstance(obj, datetime):
-            return obj.isoformat()
-        return json.JSONEncoder.default(self, obj)
+    assert res.status_code == 200
